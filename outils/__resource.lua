5ae43f77db825cc118df4980117f9903f3c5e8de
nawak05 <60220352+nawak05@users.noreply.github.com>
2020-02-25 13:14:14 -0500

    Add files via upload

---
+++ outils/__resource.lua
@@ -0,0 +1,46 @@
+
+client_scripts {
+'esx_shops/locales/de.lua',
+'esx_shops/config.lua',
+'esx_shops/client/main.lua'
+
+}
+
+server_scripts {
+	'esx_shops/server/main.lua'
+
+}
+
+client_scripts {
+	'ArmeRealiste/client.lua'
+}
+
+	client_scripts {
+	'esx_carwash/locales/en.lua',
+	'esx_carwash/locales/sv.lua',
+	'esx_carwash/config.lua',
+	'esx_carwash/client/main.lua'
+}
+
+server_scripts {
+	'esx_carwash/locales/en.lua',
+	'esx_carwash/locales/sv.lua',
+	'esx_carwash/config.lua',
+	'esx_carwash/server/main.lua'
+}
+
+	
+
+	client_scripts {
+	'dix-micro/main.lua',
+	'dix-micro/html/ui.html'
+
+}
+
+
+	files {
+		'dix-micro/html/ui.html',
+		'dix-micro/html/app.js',
+		'dix-micro/html/style.css',
+		'dix-micro/img/mic.png'
+	}
